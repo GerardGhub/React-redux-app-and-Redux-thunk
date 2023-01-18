d94be462c030dd6700a97c8359fee1c863aec867
GerardGhub <singian28@gmail.com>
2023-01-18 21:06:43 +0800

    Initial State

---
+++ src/data/tasks.js
@@ -0,0 +1,12 @@
+export const initialTask = [
+    {
+        id: 1,
+        taskTitleL: "Appointment with Bob",
+        taskDateTime: "2022-01-18 9:30"
+    },
+    {
+        id: 2,
+        taskTitleL: "Project Deployment",
+        taskDateTime: "2022-01-18 9:30"
+    },
+];
