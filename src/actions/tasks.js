40e747a08be320315b870e4ce97e707df39b1e56
GerardGhub <singian28@gmail.com>
2023-01-20 20:41:43 +0800

    Install Axios and remove fetch api

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,20 +1,16 @@
 import * as actionTypes from "../constants/action-types";
+import axios from "axios";
 
 export const fetchTasks = () => async (dispatch, getState) => {
 
-    var response = await fetch("http://localhost:7000/tasks", {
-        method: "GET"
-    });
+    var response = await axios.get("http://localhost:7000/tasks");
 
-    //read response body
-    var responseData = await response.json();
     // console.log(responseData);
-
 
     //dispatch response data to FETCH_TASKS action; it will be received by the reducer
     dispatch({
         type: actionTypes.FETCH_TASKS,
-        payload: responseData
+        payload: response.data
     });
 };
 
