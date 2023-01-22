8bff9ea715a2954caf394fc7d1fc74a5ccdf7a5a
GerardGhub <singian28@gmail.com>
2023-01-22 12:08:45 +0800

    Installing redux Logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import allReducers from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-import reduxThunk  from "redux-thunk";
+import reduxThunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 
+var logger = createLogger();
 
-var store = createStore(allReducers, composeWithDevTools(applyMiddleware(reduxThunk)));
+var store = createStore(allReducers, composeWithDevTools
+    (applyMiddleware(reduxThunk, logger)));
 export default store;
